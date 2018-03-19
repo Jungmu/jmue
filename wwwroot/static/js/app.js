4c8c5d2672a2ef0c4d2c63cb908dc4a8cb64b72a
jungmu <whdrjs0@gmail.com>
2018-03-19 11:50:05 +0900

    nav edit

--- wwwroot/static/js/app.js
+++ wwwroot/static/js/app.js
@@ -4,18 +4,3 @@ new Vue({
         message: 'Hello Vue.js!'
     }
 })
-
-var navVue = new Vue({
-    el: '#nav',
-    data: {
-        show: true
-    }
-})
-
-function navVueShow() {
-    if(window.innerHeight < 1080) {
-        navVue.show = false
-    }
-}
-document.getElementById("nav").onload = function() {navVueShow()};
-document.getElementById("nav").onresize = function(){navVueShow()};
