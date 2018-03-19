b4a0922f44e4aa22c3063fed8b703de31c8f8bab
jungmu <whdrjs0@gmail.com>
2018-03-19 17:07:04 +0900

    ssl test

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"jmue/jmueConst"
 	"jmue/logger"
 	"jmue/staticHandler"
+	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 var (
@@ -26,9 +30,25 @@ func main() {
 	defer fpLog.Close()
 	logger.InitLogger(fpLog, mode)
 
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("jmue.xyz"), //Your domain here
+		Cache:      autocert.DirCache("certs"),         //Folder for storing certificates
+	}
+
 	http.Handle("/", new(staticHandler.Handler))
 	http.Handle("/static", http.FileServer(http.Dir("wwwroot/static")))
-	http.ListenAndServe(":8080", nil)
+
+	server := &http.Server{
+		Addr: ":https",
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+
+	go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+
+	log.Fatal(server.ListenAndServeTLS("/etc/letsencrypt/live/jmue.xyz/fullchain.pem", "/etc/letsencrypt/live/jmue.xyz/privkey.pem")) //Key and cert are coming from Let's Encrypt
 }
 
 func checkMode() {
