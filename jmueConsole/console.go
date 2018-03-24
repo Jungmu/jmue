979f84179f24ecd1d38f7f288b3be28fdac23706
jungmu <whdrjs0@gmail.com>
2018-03-25 02:22:24 +0900

    sass hot reload

--- jmueConsole/console.go
+++ jmueConsole/console.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 var (
@@ -14,9 +16,41 @@ var (
 func main() {
 	pushCount = 0
 
+	go hotReloadSass()
+
 	http.HandleFunc("/restart", serverRestart)
 	http.HandleFunc("/gitPush", gitPushHandler)
 	http.ListenAndServe(":3000", nil)
+}
+
+func hotReloadSass() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+					execCommand("sassBuild.bat")
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("../scss")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
 }
 
 func serverRestart(w http.ResponseWriter, req *http.Request) {
@@ -34,7 +68,6 @@ func serverRestart(w http.ResponseWriter, req *http.Request) {
 func gitPushHandler(w http.ResponseWriter, req *http.Request) {
 	pushCount++
 	execCommand("gitPull.bat")
-	execCommand("sassBuild.bat")
 	w.Header().Add("Content-Type", "text/html")
 	w.Write([]byte("<h1>git pull complete</h1><br>"))
 	http.Redirect(w, req, "http://jmue.xyz", 301)
