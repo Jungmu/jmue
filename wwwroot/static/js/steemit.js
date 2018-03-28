17d2a5e1af25bf432d5fc4679de69d5774e8f6f3
jungmu <whdrjs0@gmail.com>
2018-03-28 16:05:26 +0900

    스팀잇에서 내가 포스팅한 글 긁어오기 - 카테고리가 dev 인 것만 긁어온다.

--- wwwroot/static/js/steemit.js
+++ wwwroot/static/js/steemit.js
@@ -20,7 +20,11 @@ var steemit = new Vue({
     }
 })
 
-steem.api.getDiscussionsByBlog({"tag": "jungmu", "limit": 20}, function(err, result) {
+var lastAuthor;
+var lastPermlink;
+
+steem.api.getDiscussionsByBlog({"tag": "jungmu", "limit": 5}, function(err, result) {
+    document.getElementById("postLoader").style.display = "block";
     result.forEach(element => {
         if(element.author == "jungmu")
         {
@@ -28,7 +32,37 @@ steem.api.getDiscussionsByBlog({"tag": "jungmu", "limit": 20}, function(err, res
             item.title = element.title;
             item.content = converter.makeHtml(element.body);
             steemit.addToPost(item);
+            lastAuthor=element.author;
+            lastPermlink = element.permlink;
+            document.getElementById("postLoader").style.display = "none";
+            return;
         }
     });
-    console.log(steemit.posts);    
-  });
+    document.getElementById("postLoader").style.display = "none";
+});
+
+function getPostMore(){
+    document.getElementById("postLoader").style.display = "block";
+    steem.api.getDiscussionsByBlog({"tag": "jungmu", "limit": 5, "start_author": lastAuthor, "start_permlink": lastPermlink}, function(err, result) {
+        result.forEach(element => {
+            if(element.author == "jungmu" && element.permlink != lastPermlink && element.category == "dev")
+            {                
+                let item = {title:"", content:""};
+                item.title = element.title;
+                item.content = converter.makeHtml(element.body);
+                steemit.addToPost(item);
+                lastAuthor=element.author;
+                lastPermlink = element.permlink;
+                document.getElementById("postLoader").style.display = "none";
+                return;
+            }
+        });
+        document.getElementById("postLoader").style.display = "none";
+    });
+}
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        getPostMore();
+    }
+};
